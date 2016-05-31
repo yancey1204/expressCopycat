07c94f3d4845908ed190d2b894fac8fd5e830cbc
yancey1204 <yancey1204@gmail.com>
2016-05-31 08:49:19 +0800

    change to class

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const expressCopycat = require('./lib/expresscopycat');
-const app = expressCopycat;
+// const expressCopycat = require('./lib/expresscopycat');
+// const app = expressCopycat;
 
-// const ExpressLite = require('./lib/expressLite');
-// const app = new ExpressLite();
+const ExpressLite = require('./lib/expressLite');
+const app = new ExpressLite();
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-
 app.get('/', (req, res) => {
-  res.end();
+  res.statusCode = 200;
+  console.log(`Got response: ${res.statusCode}`);
+  res.send('index');
 });
 
-app.get('/home', (res) => {
+app.get('/home', (req, res) => {
+  res.statusCode = 200;
   console.log(`Got response: ${res.statusCode}`);
-  console.log('home');
+  res.send('home');
 });
 
 app.listen(port, hostname);
